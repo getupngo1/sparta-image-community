d718dd268903385e4006fb330e16390195fd4b71
getupngo <dlehgod6840@gmail.com>
2021-09-30 17:22:19 +0900

    frame

---
+++ src/components/Post.js
@@ -0,0 +1,29 @@
+//Post.js
+import React from "react";
+
+const Post =(props) =>{
+    
+    return (
+        <React.Fragment>
+            <div>
+                user profile / user name / insert_dt 
+            </div>
+            <div>contents</div>
+            <div>image</div>
+            <div>comment cnt</div>
+        </React.Fragment>
+    )
+}
+
+Post.defaultProps = {
+    user_info: {
+        user_name : "getupngo",
+        user_profile: "https://t1.daumcdn.net/cfile/blog/246BD24252FF853D15"
+    },
+    image_url:"https://t1.daumcdn.net/cfile/blog/246BD24252FF853D15",
+    contents: "강아지네요!",
+    Comment_cnt: 10,
+    insert_dt: "2021-02-27 10:00:00",
+};
+
+export default Post
