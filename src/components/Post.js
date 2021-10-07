65054215496fc8cd497573022719cfa9fbbc68e0
getupngo <dlehgod6840@gmail.com>
2021-10-07 22:05:21 +0900

    week 5 completed

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,8 +8,8 @@ import { history } from "../redux/configureStore";
 
 import { Button, Grid, Image, Text } from "../elements";
 
-const Post = (props) => {
-  console.log(props);
+const Post = React.memo((props)=>{
+  console.log('hi! im post component!')
   return (
     <React.Fragment>
       <Grid>
@@ -43,7 +43,46 @@ const Post = (props) => {
       </Grid>
     </React.Fragment>
   );
-};
+});
+
+// const Post = (props) => {
+//   console.log(props);
+
+//   console.log('hi! im post component!')
+//   return (
+//     <React.Fragment>
+//       <Grid>
+//         <Grid is_flex padding="16px">
+//           <Grid is_flex width="auto">
+//             <Image shape="circle" src={props.src} />
+//             <Text bold>{props.user_info.user_name}</Text>
+//           </Grid>
+//           <Grid is_flex width="auto">
+//             <Text>{props.insert_dt}</Text>
+//             {props.is_me && (
+//               <Button width="auto" margin="4px" padding="4px" _onClick={()=>{
+//                   history.push(`/write/${props.id}`);
+//               }}>
+//                 수정
+//               </Button>
+//             )}
+//           </Grid>
+//         </Grid>
+//         <Grid padding="16px">
+//           <Text>{props.contents}</Text>
+//         </Grid>
+//         <Grid>
+//           <Image shape="rectangle" src={props.image_url} />
+//         </Grid>
+//         <Grid padding="16px">
+//           <Text margin="0px" bold>
+//             댓글 {props.comment_cnt}개
+//           </Text>
+//         </Grid>
+//       </Grid>
+//     </React.Fragment>
+//   );
+// };
 
 Post.defaultProps = {
   user_info: {
